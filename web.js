52c4fb17382771fe4f6a862d26681185d88556ce
Akinmade Adebayo <luq_mi3@yahoo.com>
2013-07-16 02:01:32 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var express = require('express');
-var app = express();
+var app = express.createServer(express.logger());
 var buf = new Buffer(fs.readFileSync('index.html','utf-8'));
 app.get('/', function (request, response) {
    response.send(buf.toString())
